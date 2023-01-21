60e5df96d48fd7cd3390f8791d1659361af5ca73
Remix GitHub Token <anthony.nnaemeka.umeh@gmail.com>
2023-01-21 10:11:46 +0100

    While and infinite loops.

--- playaround/Add.sol
+++ playaround/Add.sol
@@ -19,7 +19,7 @@ contract W {
         }
     }
 
-    function loop(uint8 n) public pure returns (uint256) {
+    function forLoop(uint8 n) public pure returns (uint256) {
         assembly {
             let limit := n
             let sum
@@ -27,6 +27,32 @@ contract W {
                 sum := add(sum, add(i, n))
             }
             mstore(0x90, sum)
+            return(0x90, 0x20)
+        }
+    }
+
+    function whileLoop(uint8 n) public pure returns (uint256) {
+        assembly {
+            let sum
+            let i
+            for {} lt(i, n) {} {
+                sum := add(sum, add(i, n))
+                i := add(i, 1)
+            }
+            mstore(0x91, sum)
+            return(0x90, 0x20)
+        }
+    }
+
+    function infiniteLoop() public pure {
+        assembly {
+            let sum
+            let i
+            for {} gt(i, 2) {} {
+                sum := add(sum, add(i, sum))
+                i := add(i, 1)
+            }
+            mstore(0x91, sum)
             return(0x90, 0x20)
         }
     }
